93065d9e6d93b74a9f6fccd0fe735567b3d89501
Colinr1994 <colinr1994@gmail.com>
2020-07-13 21:41:31 -0700

    Beans not working

--- src/main/java/cst438hw3/service/CityService.java
+++ src/main/java/cst438hw3/service/CityService.java
@@ -1,41 +1,14 @@
 package cst438hw3.service;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import java.util.TimeZone;
-
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import cst438hw3.domain.*;
 
 @Service
 public class CityService {
-	
-	// ------------------ HW3 Addition Start ------------------
-	
-	@Autowired
-	private RabbitTemplate rabbitTemplate;
-	
-	@Autowired
-	private FanoutExchange fanout;
-	
-	public void requestReservation(
-			String cityName,
-			String level,
-			String email) {
-		String msg  = "{\"cityName\": \""+ cityName +
-				"\" \"level\": \""+level+
-				"\" \"email\": \""+email+"\"}" ;
-		System.out.println("Sending message:" + msg);
-		rabbitTemplate.convertSendAndReceive(
-				fanout.getName(),
-				"", msg);
-	}
-	
 	
 	@Autowired
 	private CityRepository cityRepository;
@@ -45,18 +18,39 @@ public class CityService {
 	
 	@Autowired
 	private WeatherService weatherService;
-	
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
+	@Autowired
+	private FanoutExchange fanout;
+
+
 	public CityInfo getCityInfo(String cityName) {
-		
+
 		List<City> city = cityRepository.findByName(cityName);
-		
-		if (city.size() == 0) return null;
-		else {
-			City tempCity = city.get(0);
-			Country tempCountry = countryRepository.findByCode(tempCity.getCountryCode());
-			TempAndTime tempAndTime = weatherService.getTempAndTime(cityName);
-			return new CityInfo(tempCity, tempCountry.getName(), tempAndTime.getFormTemp(), tempAndTime.getFormTime());
+		// check to see if there is even a return
+		if (city.size() == 0) {
+			return null;
 		}
+		City tempCity = city.get(0);
+		Country country = countryRepository.findByCode(tempCity.getCountryCode());
+		TempAndTime weather = weatherService.getTempAndTime(cityName);
+
+		return new CityInfo(tempCity, country.getName(), weather.getFormTemp(), weather.getFormTime());
 	}
-	
+
+	public void requestReservation(
+			String cityName,
+			String level,
+			String email) {
+		String msg  = "{\"cityName\": \""+ cityName +
+				"\" \"level\": \""+level+
+				"\" \"email\": \""+email+"\"}" ;
+		System.out.println("Sending message:"+msg);
+		rabbitTemplate.convertSendAndReceive(
+				fanout.getName(),
+				"",   // routing key none.
+				msg);
+	}
 }

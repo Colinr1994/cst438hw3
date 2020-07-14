eeb610e5274ea0bdd892cc537d84d2c1ad683b3e
Colinr1994 <colinr1994@gmail.com>
2020-07-13 22:03:01 -0700

    Dazed and confused

--- src/main/java/cst438hw3/configurations/ConfigPublisher.java
+++ src/main/java/cst438hw3/configurations/ConfigPublisher.java
@@ -2,11 +2,12 @@ package cst438hw3.configurations;
 
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ConfigPublisher {
-
-	@Bean
-	public FanoutExchange fanout() {
-		return new FanoutExchange("city-serservation");
-	}
+  @Bean
+  public FanoutExchange fanout() {
+    return new FanoutExchange("city-reservation");
+  }
 }
